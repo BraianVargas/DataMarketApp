37311a8b4c2ccfa735530b15425057b3aee62f2d
Braian Vargas <braianvargas1616@gmail.com>
2022-08-17 00:48:48 -0300

    adding routes for filtering offers by id

--- Controllers/Modules.py
+++ Controllers/Modules.py
@@ -1,4 +1,5 @@
 import csv
+# from Classes.offers_class import *
 from ..Classes.offers_class import Offers
 
 offersList = []
@@ -9,5 +10,14 @@ def readFile(filename):
         next(csv_reader)
         for row in csv_reader:
             offer = Offers(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15])
-            offersList.append(offer)
+            offersList.append(offer.__dict__)
     return offersList
+
+def getByOfferIds(id):
+    i = 0 
+    while i < len(offersList) and int(id) != int(offersList[i]['_Offers__offerId']):
+        i+=1
+    if i<len(offersList):
+        return i
+    else:
+        return "404 error - Offer Not Found"
--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
+from re import S
 from flask import Flask
+import json
 from .Controllers.Modules import *
 
 app = Flask(__name__)
@@ -8,10 +10,19 @@ objectList = readFile('DataFiles/offers_data.csv')
 
 dictionaryList = []
 
-@app.route("/")
-def hello_world():
+@app.route("/getOffers")
+def getAll():
     return objectList
 
-if __name__=='__main__':
-    app.run(debug=True)
-    app.run()
+@app.route('/filter/offerId/<offerId>')
+def getByOfferId(offerId):
+    i = getByOfferIds(offerId)
+    if isinstance(i, int):
+        # return json.dumps(objectList[i])
+        return objectList[i]
+    else:
+        return i
+
+# if __name__=='__main__':
+#     app.run(debug=True)
+#     app.run()
