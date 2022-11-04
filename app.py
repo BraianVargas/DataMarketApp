3ff9f7ae565e6d7881a6d0cdc14097177f8efe08
Braian Vargas <braianvargas1616@gmail.com>
2022-11-04 19:12:08 -0300

    <prueba vercel>

--- app.py
+++ app.py
@@ -24,8 +24,16 @@ app.register_blueprint(questionsBP, url_prefix = '/questons')
 from App.Profile.Surveys.Answers import answersBP
 app.register_blueprint(answersBP, url_prefix = '/answers')
 
+@app.route('/indexes')
+def getUsers():
+    db, c = getDB()
+    c.execute("SELECT * FROM profile ORDER BY id ASC")
+    users = c.fetchall()
 
-
+    if users != None:
+        return users
+    else:
+        return "404 - User Table Is Empty"
 
 # ----------------------------- GENERA EL ADMINISTRADOR DE LOGIN --------------------------------------
 login_manager = LoginManager(app)
